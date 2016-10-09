5ffa627ad714dd6ecfb32193578f55597cb752da
David Lin <dlin@boomtrain.com>
2016-10-09 12:32:21 -0700

    add for loop

--- readFromFile.py
+++ readFromFile.py
@@ -6,3 +6,10 @@ for line in fhand:
     if not line.startswith('From ') : continue
     words = line.split()
 print words[2]
+
+# for loop example
+fruit = "apple"
+for letter in fruit:
+    print letter
+
+
