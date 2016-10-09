5967c14b37b2c3f92198a9d2e5fdf8a578807aa8
David Lin <dlin@boomtrain.com>
2016-10-09 11:54:20 -0700

    add readfromfile

---
+++ readFromFile.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('From ') : continue
+    words = line.split()
+print words[2]
