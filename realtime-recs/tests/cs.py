5b7abaa1ab338f6afa2c05f9a0f91bcb2b6c1bdc
David Lin <dlin@boomtrain.com>
2016-12-20 09:42:04 -0800

    adding more test cases for cs and rts

---
+++ realtime-recs/tests/cs.py
@@ -0,0 +1,57 @@
+from bt_candidates.wiring import default_schema_factory, default_filter_factory as f
+from bt_candidates.client import Client
+from bt_candidates.sorting import SortStrategy
+import pytest
+
+CLIENT = Client(host='candidates.aws.boomtrain.com', port=7070)
+
+testdata = [
+    ('WireFly', '92d7386bbdaae2999f701aa2a614eaeb'),
+    ('IEEE', 'fc09a30ba1a4b43d0cc9990be2df89bb'),
+    ('Vogue', '9b69d8fc8b441b43d493d713e5703ada'),
+    ('THG', 'fe3d1b4f09f60315d2bbfb27557a10e3'),
+    ('Rappler', '1a1e951c0be6ac5f7a57c617f1160972'),
+    ('Kellogg Insight', '2a9897b9f56088c2916bb3403cfff631')
+]
+
+@pytest.mark.parametrize("customer_name, site_id", testdata)
+def test_no_filter(customer_name, site_id):
+    candidates = CLIENT.get_candidates(site_id, filter=None, limit=100, sort_by=SortStrategy.POP_1D)
+    assert len(candidates) > 0
+
+@pytest.mark.parametrize("customer_name, site_id", testdata)
+def test_global_filter(customer_name, site_id):
+    client = Client(host='candidates.aws.boomtrain.com', port=7070)
+    candidates = client.get_candidates(site_id, filter=f.named_filter('GLOBAL'), limit=100, sort_by=SortStrategy.POP_1D)
+    assert len(candidates) > 0
+
+@pytest.mark.parametrize("customer_name, site_id", testdata)
+def test_metafilter_resource_type_article(customer_name, site_id):
+    filter = f.overlap_filter(field='resource-type', values=['article'], min=1)
+    candidates = CLIENT.get_candidates(site_id, filter=f.and_filter(f.named_filter('GLOBAL'), filter), limit=100, sort_by=SortStrategy.POP_1D)
+    assert len(candidates) > 0
+
+@pytest.mark.parametrize("customer_name, site_id", testdata)
+def test_metafilter_resource_type_news(customer_name, site_id):
+    filter = f.overlap_filter(field='resource-type', values=['News'], min=1)
+    candidates = CLIENT.get_candidates(site_id, filter=f.and_filter(f.named_filter('GLOBAL'), filter), limit=100, sort_by=SortStrategy.POP_1D)
+    assert len(candidates) > 0
+
+@pytest.mark.parametrize("customer_name, site_id", testdata)
+def test_metafilter_module(customer_name, site_id):
+    filter = f.overlap_filter(field='module', values=['Sponsored'], min=1)
+    candidates = CLIENT.get_candidates(site_id, filter=f.and_filter(f.named_filter('GLOBAL'), filter), limit=100, sort_by=SortStrategy.POP_1D)
+    assert len(candidates) > 0
+
+@pytest.mark.parametrize("customer_name, site_id", testdata)
+def test_metafilter_module_not(customer_name, site_id):
+    filter = f.overlap_filter(field='module', values=['Sponsored'], min=0, max=0)
+    candidates = CLIENT.get_candidates(site_id, filter=f.and_filter(f.named_filter('GLOBAL'), filter), limit=100, sort_by=SortStrategy.POP_1D)
+    assert len(candidates) > 0
+
+@pytest.mark.skip(reason="Not sure if this works for all customers")
+@pytest.mark.parametrize("customer_name, site_id", testdata)
+def test_metafilter_keywords(customer_name, site_id):
+    filter = f.overlap_filter(field='keywords', values=['Nokia', 'HMD Global'], min=1)
+    candidates = CLIENT.get_candidates(site_id, filter=f.and_filter(f.named_filter('GLOBAL'), filter), limit=100, sort_by=SortStrategy.POP_1D)
+    assert len(candidates) > 0
---
+++ realtime-recs/tests/rts.py
@@ -0,0 +1,88 @@
+import pytest
+import recs_client.request as req
+import bt_rts.thrift.gen.filters as recs_filter
+from recs_client.client import RecommendationsClient
+
+# HOST = 'localhost'
+HOST = 'rts.aws.boomtrain.com'
+PORT = 7070
+TIMEOUT = 20000
+BSIN = 'ec5e00d4-6386-4cf7-a1d1-ea0e4f7d8a8c'
+RECSET_ID = 'fakedb0c-c5c6-4515-9bd1-5a06ddd676f6'
+EMPTY_SEEDS =[]
+EMPTY_EXCLUDES =[]
+TEST = True
+GROUP_NAME = 'default'
+COUNT = 2
+CALLING_APP = 'test_client'
+
+testdata = [
+    ('WireFly', '92d7386bbdaae2999f701aa2a614eaeb'),
+    ('IEEE', 'fc09a30ba1a4b43d0cc9990be2df89bb'),
+    ('Vogue', '9b69d8fc8b441b43d493d713e5703ada'),
+    ('THG', 'fe3d1b4f09f60315d2bbfb27557a10e3'),
+    ('Rappler', '1a1e951c0be6ac5f7a57c617f1160972'),
+    ('Kellogg Insight', '2a9897b9f56088c2916bb3403cfff631')
+]
+
+
+@pytest.mark.parametrize("customer_name, site_id", testdata)
+def test_no_filter(customer_name, site_id):
+
+    request = req.RecsRequest(site_id=site_id,
+                              bsin=BSIN,
+                              seeds=EMPTY_SEEDS,
+                              excludes=EMPTY_EXCLUDES,
+                              recset_id=RECSET_ID,
+                              test=TEST)
+
+    request.groups[GROUP_NAME] = req.RecGroupRequest(count=COUNT)
+    config = {'host': HOST, 'port': PORT, 'timeout': TIMEOUT}
+    with RecommendationsClient(calling_app=CALLING_APP, **config) as client:
+        response = client.get_recommendations(request)
+    assert len(response) == COUNT
+
+@pytest.mark.parametrize("customer_name, site_id", testdata)
+def test_global_filter(customer_name, site_id):
+
+    request = req.RecsRequest(site_id=site_id,
+                              bsin=BSIN,
+                              seeds=EMPTY_SEEDS,
+                              excludes=EMPTY_EXCLUDES,
+                              recset_id=RECSET_ID,
+                              test=TEST)
+
+
+    filter = recs_filter.TFilter(and_=None, existence=None, range=None, recency=None,
+                                 or_=None, named='GLOBAL', overlap=None)
+    request.groups[GROUP_NAME] = req.RecGroupRequest(count=COUNT, metafilter=filter)
+    config = {'host': HOST, 'port': PORT, 'timeout': TIMEOUT}
+    with RecommendationsClient(calling_app=CALLING_APP, **config) as client:
+        response = client.get_recommendations(request)
+    assert len(response) == COUNT
+
+@pytest.mark.parametrize("customer_name, site_id", testdata)
+def test_metafilter(customer_name, site_id):
+
+    request = req.RecsRequest(site_id=site_id,
+                              bsin=BSIN,
+                              seeds=EMPTY_SEEDS,
+                              excludes=EMPTY_EXCLUDES,
+                              recset_id=RECSET_ID,
+                              test=TEST)
+
+
+    metafilter = recs_filter.TFilter(overlap=None, recency=None, and_=[
+        recs_filter.TFilter(overlap=None, recency=None, and_=None, existence=None, or_=None, named='GLOBAL', range=None),
+        recs_filter.TFilter(overlap=recs_filter.TOverlapFilter(values=['NONE'], field='allowed_territories', amount=recs_filter.TRange(min_=1.0, max_=None),
+                                       match_type=0), recency=None, and_=None, existence=None, or_=None, named=None,
+                range=None),
+        recs_filter.TFilter(overlap=recs_filter.TOverlapFilter(values=['DE'], field='resource-type', amount=recs_filter.TRange(min_=1.0, max_=None),
+                                       match_type=0), recency=None, and_=None, existence=None, or_=None, named=None,
+                range=None)], existence=None, or_=None, named=None, range=None)
+
+    request.groups[GROUP_NAME] = req.RecGroupRequest(count=COUNT, metafilter=metafilter)
+    config = {'host': HOST, 'port': PORT, 'timeout': TIMEOUT}
+    with RecommendationsClient(calling_app=CALLING_APP, **config) as client:
+        response = client.get_recommendations(request)
+    assert len(response) == COUNT
